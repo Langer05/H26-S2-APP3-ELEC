fc13653b76bf9d881b47dcd5cf7d7433fae18421
Langer05 <belangerdpro@gmail.com>
2026-02-02 12:08:16 -0500

    ef

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import argparse as ap               # Entree de parametres
 data1_file_name = './S2GE_APP3_Problematique_Detecteur_Primaire.csv'
 data2_file_name = './S2GE_APP3_Problematique_Detecteur_Secondaire.csv'
 
-data1 = np.loadtxt(data1_file_name, delimiter=',', skiprows=1)
-data2 = np.loadtxt(data2_file_name, delimiter=',', skiprows=1)
-hist_bins = 20
+data1 = np.loadtxt(data1_file_name, delimiter=',', skiprows=1) # Load les donnees primaires dans une matrice
+data2 = np.loadtxt(data2_file_name, delimiter=',', skiprows=1) # Load les donnees secondaires dans une matrice
+hist_bins = 20 # Valeur nombre de bins dans l'histogramme
 
 data1_volt_max = data1[0][2]
 data1_volt_min = data1[0][2]
@@ -19,12 +19,12 @@ for i in range(len(data1)):
         data1_volt_min = data1[i][2]
 print(data1_volt_max, data1_volt_min)
 
-data1_hist_range = (data1_volt_max - data1_volt_min)/hist_bins
 data1_volt = np.zeros(len(data1))
 for i in range(len(data1)):
     data1_volt[i] = data1[i][2]
 
-counts, bins = np.histogram(data1_volt)
+data1_hist_range = (data1_volt_max - data1_volt_min)/hist_bins
+counts, bins = np.histogram(data1_volt, bins=hist_bins)
 plt.figure(figsize=(10,10))
 plt.stairs(counts, bins)
 plt.show()
