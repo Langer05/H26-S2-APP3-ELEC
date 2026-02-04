8cf8f37c33b604d77e62d1bff6e6992aec7d0c44
Langer05 <belangerdpro@gmail.com>
2026-02-04 00:28:57 -0500

    .

--- main.py
+++ main.py
@@ -36,7 +36,9 @@ def make_histogram_log(amp, nb_bins = 30):
 
 def make_histogram(amp, nb_bins = 30):
     bin_width = np.linspace(amp.min(), amp.max(), nb_bins)
+    print("test -->\n",bin_width)
     hist, bin_edges = np.histogram(a = amp, bins = bin_width)
+    print(bin_edges)
     return hist, bin_edges
 
 def add_histogram(hist1, hist2, bin_edges1, bin_edges2):
@@ -49,29 +51,36 @@ def coincidence(arr1, arr2, tolerance, bin_edges):
     non_coincident = [0] * len(bin_edges)
     coincident = [0] * len(bin_edges)
 
+    i, j = 0, 0
     k = 1
-    for i in range(len(arr1)):
-        delta = (arr1[i] - arr2[i])
-        avr = (arr1[i] + arr2[i]) / 2
-
-        # What bin
-        if avr > bin_edges[k]:
-            while avr > bin_edges[k]:
-                print(i)
-                if k >= 29:
-                    break
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] > bin_edges[k] or arr2[j] > bin_edges[k]:
+            if k < 29:
                 k = k + 1
-        # Coincidence
-        if delta < tolerance:
-            coincident[k - 1] = coincident[k - 1] + 1
-        # Non-coincidence
-        else:
-            non_coincident[k - 1] = non_coincident[k - 1] + 1
+        delta = arr1[i] - arr2[j]
+        if np.abs(delta) < tolerance:
+            coincident[k - 1] += 1
+            i += 1
+            j += 1
+        if delta < 0:
+            i += 1
+        if delta > 0:
+            j += 1
+        all_event[k - 1] += 1
 
-        all_event[k - 1] = all_event[k - 1] + 1
+    for i in range(len(non_coincident)):
+        non_coincident[i] = all_event[i] - coincident[i]
+
+    total_all = np.sum(all_event)
+
+    for i in range(len(all_event)):
+        all_event[i] = all_event[i] / total_all
+    for i in range(len(coincident)):
+        coincident[i] = coincident[i] / total_all
+    for i in range(len(non_coincident)):
+        non_coincident[i] = non_coincident[i] / total_all
 
     return all_event, non_coincident, coincident
-
 
 def main():
     data1_file_name = './S2GE_APP3_Problematique_Detecteur_Primaire.csv'
@@ -87,13 +96,15 @@ def main():
 
     all_event, non_coincident, coincident = coincidence(time1, time2,
                                                         0.010, bin_edges1)
+    volt1 = get_data_file(data1_file_name, 'VOLT')
+    bin_volt1, bin_volt1_edges = make_histogram_log(volt1, nb_bins = 30)
 
     plt.figure(figsize=(10,10))
     print(len(all_event))
     print(bin_edges1.shape)
-    plt.step(bin_edges1, all_event, label="All events")
-    plt.step(bin_edges1, coincident, label="Coincident")
-    plt.step(bin_edges1, non_coincident, label="Non coincident")
+    plt.step(bin_edges1, all_event, label="All events", color='blue')
+    plt.step(bin_edges1, coincident, label="Coincident", color='red')
+    plt.step(bin_edges1, non_coincident, label="Non coincident", color='green')
     plt.xscale('log')
     plt.legend()
     plt.show()
